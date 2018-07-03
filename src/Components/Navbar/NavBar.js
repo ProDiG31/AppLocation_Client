e1be8f76036d89a4d45eec073f10348ea0dd9209
Flo <florian.chevallier@morning.com>
2018-07-03 11:13:48 +0200

    move jsx to jx and upgrade webpacké

--- src/Components/Navbar/NavBar.js
+++ src/Components/Navbar/NavBar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './NavBar.css';
 
 class NavBarComponent extends React.Component {
 
@@ -21,7 +20,7 @@ class NavBarComponent extends React.Component {
                                 <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                             </li>
                             <li className="col-sm-3">
-                                <a className="nav-link" href="#">Features</a>
+                                <a className="nav-link" href="#">Tes</a>
                             </li>
                             <li className="col-sm-3">
                                 <a className="nav-link" href="#">Pricing</a>
--- webpack.config.js
+++ webpack.config.js
@@ -3,67 +3,39 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 
-const extractSass = new ExtractTextPlugin({
-    filename: 'css/[name].css'
-  });
-  
-// const css_rules = {
-//     test: /.scss$/,
-//     exclude: /(node_modules|bower_components)/,
-//     use: extractSass.extract({
-//       use: [
-//         {
-//           loader: 'css-loader',
-//           options: {
-//             minimize: process.env.NODE_ENV === 'production'
-//           }
-//         },
-//         {
-//           loader: 'postcss-loader'
-//         },
-//         {
-//           loader: 'sass-loader'
-//         }
-//       ]
-//     })
-//   };
-
 const js_rules = {
-    test: /\.jsx?$/,
-    exclude: /(node_modules|bower_components)/,
-    loader: 'babel-loader',
-    query: {
-        presets: ['react', 'es2015', 'stage-3']
-    }
+  test: /\.js?$/,
+  exclude: /(node_modules|bower_components)/,
+  loader: 'babel-loader',
+  query: {
+    presets: ['react', 'es2015', 'stage-3']
+  }
 }
 
 module.exports = {
-    entry: './src/index.jsx',
-    output: {
-        path: path.resolve('dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules : [
-              js_rules
-            //   ,              css_rules  
-            ]   
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html',
-        inject: 'body'
-    })],
-    devServer: {
-        historyApiFallback: true
-    },
-    externals: {
-        // global app config object
-        config: JSON.stringify({
-            apiUrl: 'http://localhost:4000'
-        })
-    }
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      js_rules
+      //   ,              css_rules  
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+  })],
+  devServer: {
+    historyApiFallback: true
+  },
+  externals: {
+    // global app config object
+    config: JSON.stringify({
+      apiUrl: 'http://localhost:4000'
+    })
+  }
 }
